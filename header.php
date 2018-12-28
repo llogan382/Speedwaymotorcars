d6484aefaf8874bc2f7af8cbd94864718d559bf3
Unknown <llogan382@gmail.com>
2018-12-28 17:03:39 -0500

    Adds archive page, updates-men

--- header.php
+++ header.php
@@ -20,13 +20,19 @@
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-<nav class="navbar navbar-expand-md navbar-light bg-light" role="navigation">
+<nav class="lwd-nav navbar navbar-expand-md navbar-light" role="navigation">
   <div class="container">
+	  <div class="col-4">
+	  <?php
+			the_custom_logo();?>
+	  </div>
+	  
 	<!-- Brand and toggle get grouped for better mobile display -->
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
 	</button>
-	<a class="navbar-brand" href="#">Navbar</a>
+	<a class="navbar-brand" href="#"></a>
+
 		<?php
 		wp_nav_menu( array(
 			'theme_location'    => 'primary',
@@ -34,10 +40,11 @@
 			'container'         => 'div',
 			'container_class'   => 'collapse navbar-collapse',
 			'container_id'      => 'bs-example-navbar-collapse-1',
-			'menu_class'        => 'nav navbar-nav',
+			'menu_class'        => 'nav navbar-nav lwd-menu-ul',
 			'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
 			'walker'            => new WP_Bootstrap_Navwalker(),
 	  ) );
 	  ?>
   </div>
 </nav>
+<?php if ( function_exists( 'meteor_slideshow' ) ) { meteor_slideshow(); } ?>
